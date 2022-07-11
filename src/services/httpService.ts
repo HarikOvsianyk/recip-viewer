b34a75bc84d4bf0207e6e217809b9180011f769d
Harik Ovsianyk <ovsianyk@cp3258-lx>
2022-07-11 18:37:54 +0300

    Added to httpService method get

--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -1,8 +1,20 @@
 import axios, { AxiosResponse } from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}`,
+  baseURL: `${process.env.NEXT_APP_API_URL}`,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+export class HTTPService {
+  static get(path = ''): Promise<AxiosResponse> {
+    return axiosInstance
+      .get(`${path}`)
+      .then((response: AxiosResponse) => response.data)
+      .catch(err => {
+        throw new Error(err);
+      });
+  }
+};
+
